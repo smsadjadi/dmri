542e062171b3debe796fe21c2e9611a290f8484d
smsadjadi <s.mostafa.sadjadi@gmail.com>
2025-06-01 23:03:52 +0330

    lighter options added

--- tract.sh
+++ tract.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4a391f782fb1ca33826493cd1eaffe2c67190ac9c8707e4070e98a6f78d153a
-size 10581
+oid sha256:9f36ce88e5016b1d550a3532834c03ee8e35a848c183e720ed31d282a974e140
+size 11665
