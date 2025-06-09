d2cf6fc8471a4ce494514c2b1e34978479622e82
smsadjadi <s.mostafa.sadjadi@gmail.com>
2025-06-09 22:07:33 +0330

    major reorganize

---
+++ fsl/tract.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8eda9f8f3334ba0dd807c536f161301b30493285a56567d6f83908f70132f38
+size 14719
---
+++ nipype/tract.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a519361fa1cee98e384d9c5d8f6b6501189ec1b32748037ed4bfb744cd7c200d
+size 13718
